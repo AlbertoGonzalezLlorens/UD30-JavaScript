4660d2c444c9b09c74ec21c8b4389df6a5f1d245
AlbertoGonzalezLlorens <114727348+AlbertoGonzalezLlorens@users.noreply.github.com>
2022-11-29 19:15:39 +0100

    Ejercicio3

---
+++ Ejercicio3/codigo.js
@@ -0,0 +1,5 @@
+var month= ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+for (element of month){
+    console.log(element);
+}
+
