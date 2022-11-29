0537582c2160fdd120f01e405d868addff6a8729
AlbertoGonzalezLlorens <114727348+AlbertoGonzalezLlorens@users.noreply.github.com>
2022-11-29 19:07:05 +0100

    Ejercicio2

--- Ejercicio2/codigo.js
+++ Ejercicio2/codigo.js
@@ -3,5 +3,5 @@ console.log ("Hola Mundo!");
 //Se lanza el segundo mensaje
 console.log ("Soy el primer script");
 
-var message= "Hola Mundo!\n Qué fácil es inculir 'comillas simples' y \"comillas dobles\"";
+var message= "Hola Mundo!\nQué fácil es inculir 'comillas simples' y \"comillas dobles\"";
 alert(message);
