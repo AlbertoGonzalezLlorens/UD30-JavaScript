8c4da1123afb960701b61ec8599f1dbaadb6420d
AlbertoGonzalezLlorens <114727348+AlbertoGonzalezLlorens@users.noreply.github.com>
2022-11-30 18:56:25 +0100

    Porfavor

--- Ejercicio8/codigo.js
+++ Ejercicio8/codigo.js
@@ -1,7 +1,7 @@
-var numero = window.prompt("introducir un valor entero");
+var numero = prompt("introducir un valor entero");
 
 function espar(numero){
-    if(numero%2==0){
+    if(numero%2 == 0){
         return "El resultado es par";
     }else{
         return "El resultado es impar";
