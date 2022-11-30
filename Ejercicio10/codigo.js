43df22d065bbd885391ac04b8d57955922c41f28
AlbertoGonzalezLlorens <114727348+AlbertoGonzalezLlorens@users.noreply.github.com>
2022-11-30 19:31:05 +0100

    Ejercicio10

---
+++ Ejercicio10/codigo.js
@@ -0,0 +1,18 @@
+function comprobarpal(texto){
+    var reves;
+    texto=texto.toLowerCase();
+    texto=texto.replace(/ /g,'');
+    for(i=texto.length();i>=0;i--){
+        reves+=texto[i-1];
+    }
+    console.log(texto);
+    console.log(reves);
+    if(texto==reves){
+        return "palíndramo"
+    }else{
+        return "no es palíndramo"
+    }
+}
+
+var texto = prompt("introducir un texto");
+alert(comprobarpal(texto));
